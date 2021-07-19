fcd86e67343dd90a435018584d78bd39402eee52
miqunore <82279998+miqunore@users.noreply.github.com>
2021-07-19 11:32:12 +0300

    dz1

---
+++ script.js
@@ -0,0 +1,35 @@
+let day = 5;
+if (day <= 10) {
+    console.log('first decade');
+} else if (day <= 20) {
+    console.log('second decade');
+} else {
+    console.log('third decade');
+}
+
+
+let month = 9;
+
+if (month === 1 || month === 2 || month === 12) {
+    console.log('зима');
+} else if (month === 3 || month === 4 || month === 5) {
+    console.log('весна');
+} else if (month === 6 || month === 7 || month === 8) {
+    console.log('лето');
+}
+    else {
+        console.log('осень');
+    }
+
+
+let min = 55;
+if (min <= 15) {
+    console.log('первая четверть');
+} else if (min <= 30) {
+    console.log('вторая четверть');
+} else if (min <= 45) {
+    console.log('третья четверть');
+} else {
+    console.log('четвертая четверть');
+}
+
